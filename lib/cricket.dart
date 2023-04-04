1f857b445ec702f904f389fec612befccd3188d0
Ayushftw <97087155+Ayushftw@users.noreply.github.com>
2023-04-04 14:19:13 +0530

    Update cricket.dart

--- lib/cricket.dart
+++ lib/cricket.dart
@@ -2,54 +2,56 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Live Cricket Scores',
-      home: HomePage(),
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class HomePage extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String _liveScores = "";
+class _MyAppState extends State<MyApp> {
+  String _score = '';
 
-  Future<void> _getLiveScores() async {
+  Future<void> _getScore() async {
     final response = await http.get(Uri.parse(
-        'https://api.cricketapi.com/cricket_score?apikey=f35a416e-a4de-461c-99f7-75a2b2386570'));
+        'https://api.cricapi.com/v1/countries?apikey=f35a416e-a4de-461c-99f7-75a2b2386570'));
     if (response.statusCode == 200) {
+      final data = json.decode(response.body);
       setState(() {
-        _liveScores = json.decode(response.body)['data']['score'];
+        _score = data['score'];
       });
     } else {
-      throw Exception('Failed to load live scores');
+      setState(() {
+        _score = 'Error: ${response.statusCode}';
+      });
     }
   }
 
   @override
-  void initState() {
-    super.initState();
-    _getLiveScores();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Live Cricket Scores'),
-      ),
-      body: Center(
-        child: Text(
-          _liveScores,
-          style: TextStyle(fontSize: 24),
+    return MaterialApp(
+      title: 'Live Cricket Score',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Live Cricket Score'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '$_score',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: _getScore,
+                child: Text('Refresh Score'),
+              ),
+            ],
+          ),
         ),
       ),
     );
